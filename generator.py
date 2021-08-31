19755ec52c350c06a103bad4fefb1116c7d80904
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-08-30 22:06:24 -0500

    - Add version numbers for each change - Rename variable names

--- generator.py
+++ generator.py
@@ -1,24 +1,37 @@
-import os
 from datetime import datetime
+from os import path, system
 
-os.system(f'git log --reverse > commits.txt')
+versions = []
+for n in range(1, 10):
+    ver = str(n)
+    if len(ver) == 1:
+        versions.append(f'0.{ver}')
+    elif len(ver) == 2:
+        versions.append(f'{ver[0]}.{ver[1]}')
 
-with open('commits.txt', 'r') as file:
+system('git log --reverse > source_change_log.txt')
+
+with open('source_change_log.txt', 'r') as file:
     log = file.read().splitlines()
 
-for index, s in enumerate(log):
-    s = s.strip()
-    if not s.startswith('commit') and not s.startswith('Author'):
-        if s.startswith('Date'):
-            ind = log.index(s)
-            # log.pop(ind + 1)
-            s = ' '.join(s.lstrip('Date:').strip().split()[0:-1])
-            datetime_obj = datetime.strptime(s, "%a %b %d %H:%M:%S %Y")
-            s = datetime_obj.strftime("%m/%d/%Y")
-            log[ind + 1] = '-' * len(s)
-        elif s:
-            if s[0].isdigit():
-                s = s.replace(s[0:2], '-')
-            if not s[0] == '-':
-                s = f'- {s}'
-        print(s)
+if path.isfile('CHANGELOG'):
+    system('rm CHANGELOG')
+n = 0
+with open('CHANGELOG', 'a') as file:
+    for index, element in enumerate(log):
+        element = element.strip()
+        if not element.startswith('commit') and not element.startswith('Author'):
+            if element.startswith('Date'):
+                ind = log.index(element)
+                element = ' '.join(element.lstrip('Date:').strip().split()[0:-1])
+                datetime_obj = datetime.strptime(element, "%a %b %d %H:%M:%S %Y")
+                element = f'{versions[n]} ({datetime_obj.strftime("%m/%d/%Y")})'
+                n += 1
+                log[ind + 1] = '-' * len(element)
+                # log.pop(ind + 1)  # Use this leave it with a blank line instead of '----'
+            elif element:
+                if element[0].isdigit():
+                    element = element.replace(element[0:2], '-')
+                if not element[0] == '-':
+                    element = f'- {element}'
+            file.write(element + '\n')
